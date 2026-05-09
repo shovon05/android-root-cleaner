1b7697468588677870046303190ac013bc48e4fb
Shovon <155237866+shovon05@users.noreply.github.com>
2026-05-09 19:46:29 +0600

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+su -c '
+pm trim-caches 999G
+find /data/data/*/cache -type f -delete 2>/dev/null
+find /data/user/0/*/cache -type f -delete 2>/dev/null
+rm -rf /data/local/tmp/*
+rm -rf /cache/*
+rm -rf /sdcard/DCIM/.thumbnails/*
+find /data/log -type f -delete 2>/dev/null
+rm -rf /data/ota_package/* 2>/dev/null
+rm -rf /cache/recovery/* 2>/dev/null
+find /sdcard -type d -empty -delete 2>/dev/null
+echo "Cleanup complete."
+'
